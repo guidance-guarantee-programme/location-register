48f3397e1b03c326dee8dc42a4b6886666e7e141
David Henry <dw_henry@yahoo.com.au>
2016-03-21 10:51:53 +0000

    Location locations JSON file into system
    This is to keep the git history parsing code out of the main codebase.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+require 'json'
+Address.delete_all
+Location.delete_all
+
+File.open(Rails.root.join('db/seeds/locations.json')) do |f|
+  while (line = f.readline).present? do
+    print '.'
+    location_params = JSON.parse(line)
+    address_params = location_params.delete('address')
+
+    address = Address.find_by(uid: address_params['uid']) ||
+      Address.create!(address_params)
+
+    # as some of the historical data does not meet new validation requirements
+    Location.new(location_params.merge(address: address))
+      .save(validate: location_params['state'] == 'current')
+    break if f.eof?
+  end
+end
+puts ''
