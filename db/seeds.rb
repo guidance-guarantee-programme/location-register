0d3613552a945cc0dcc77eab09c8647ca40e797d
David Henry <dw_henry@yahoo.com.au>
2016-04-05 10:57:37 +0100

    Remove initial database seeding code

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,28 +6,3 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-require 'json'
-Address.delete_all
-Location.delete_all
-
-def get_address(params, state)
-  address = Address.find_by(uid: params['uid'])
-  address.update_attributes(params) if address && state == 'current'
-  address || Address.create!(params)
-end
-
-File.open(Rails.root.join('db/seeds/locations.json')) do |f|
-  while !f.eof? do
-    print '.'
-    line = f.readline
-    location_params = JSON.parse(line)
-    address_params = location_params.delete('address')
-
-    address = get_address(address_params, location_params['state'])
-
-    # as some of the historical data does not meet new validation requirements
-    location = Location.new(location_params.merge(address: address))
-    location.save!(validate: location_params['state'] == 'current')
-  end
-end
-puts ''
