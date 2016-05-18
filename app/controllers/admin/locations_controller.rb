30cb13437017fbd01a33abce89a44f9eae985e8c
David Henry <dw_henry@yahoo.com.au>
2016-05-18 11:41:33 +0100

    Extract LocationsDirectory form object
    This extracts boolean flag issues out of the
    controller. This was required due to CSV filtering
    not correctly set 'false' to false.

--- app/controllers/admin/locations_controller.rb
+++ app/controllers/admin/locations_controller.rb
@@ -4,19 +4,10 @@ module Admin
 
     def index
       authorize Location
-      scope = Location.with_visibility_flags(hidden_flags)
-
+      @locations_directory = LocationsDirectory.new(current_user, params)
       respond_to do |format|
-        format.html do
-          @locations, @sorting_params = policy_scope(scope).alpha_paginate(
-            params[:letter],
-            ALPHABETICAL_PAGINATE_CONFIG.dup,
-            &:title
-          )
-        end
-        format.csv do
-          render csv: LocationsCsv.new(policy_scope(scope))
-        end
+        format.html { @locations, @sorting_params = @locations_directory.paginated_locations }
+        format.csv { render csv: LocationsCsv.new(@locations_directory.locations) }
       end
     end
 
@@ -43,21 +34,6 @@ module Admin
     def set_autherised_location
       @location ||= Location.current.find_by!(uid: params[:id])
       authorize @location
-    end
-
-    def hidden_flags
-      [].tap do |flags|
-        flags << true if display_hidden?
-        flags << false if display_active?
-      end
-    end
-
-    def display_active?
-      params.fetch(:display_active, true).present?
-    end
-
-    def display_hidden?
-      params[:display_hidden].present?
     end
   end
 end
