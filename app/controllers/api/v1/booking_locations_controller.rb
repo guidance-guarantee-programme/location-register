53d1625446c84c836412b4bc3c476fbcebe9106a
Ben Lovell <benjamin.lovell@gmail.com>
2016-06-10 16:47:38 +0100

    Only permit signon authorised accounts

--- app/controllers/api/v1/booking_locations_controller.rb
+++ app/controllers/api/v1/booking_locations_controller.rb
@@ -1,6 +1,10 @@
 module Api
   module V1
     class BookingLocationsController < ActionController::Base
+      include GDS::SSO::ControllerMethods
+
+      before_action :require_signin_permission!
+
       def show
         @location = Location.booking_location_for(params[:uid])
       end
