256507b962b962de935cd68f0ea5a7a40afb4ac8
David Henry <dw_henry@yahoo.com.au>
2016-08-19 10:55:59 +0100

    Ensure old twilio number is redirected correctly
    When a twilio number is updated, the old number
    should continue to work and redirect to the
    current phone number for the location.

---
+++ spec/models/twilio_redirect_spec.rb
@@ -0,0 +1,48 @@
+require 'rails_helper'
+
+RSpec.describe TwilioRedirection do
+  describe '.for' do
+    let(:twilio_number) { '+44123456789' }
+    let!(:location) { create(:location, twilio_number: twilio_number, hidden: hidden) }
+
+    context 'when the twilio number exists on a current location' do
+      context 'and the location is active' do
+        let(:hidden) { false }
+
+        it 'redirects to the locations phone number' do
+          expect(described_class.for(twilio_number).phone).to eq(location.phone)
+        end
+      end
+
+      context 'and the location is hidden' do
+        let(:hidden) { true }
+
+        it 'redirects to the call centre' do
+          expect(described_class.for(twilio_number).phone).to eq(::Location::TP_CALL_CENTRE_NUMBER)
+        end
+      end
+    end
+
+    context 'when the twilio number exists on a old location' do
+      let(:user) { create(:user) }
+      let(:params) { { 'twilio_number' => '+44987654321', 'phone' => '+44111222333' } }
+      let!(:current_location) { CreateOrUpdateLocation.new(location: location, user: user).update(params) }
+
+      context 'and the current version of the location is active' do
+        let(:hidden) { false }
+
+        it "redirects to the current location's phone number" do
+          expect(described_class.for(twilio_number).phone).to eq(current_location.phone)
+        end
+      end
+
+      context 'and the current version of the location is hidden' do
+        let(:hidden) { true }
+
+        it 'redirects to the call centre' do
+          expect(described_class.for(twilio_number).phone).to eq(::Location::TP_CALL_CENTRE_NUMBER)
+        end
+      end
+    end
+  end
+end
