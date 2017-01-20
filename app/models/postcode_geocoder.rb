61e6ca492082d86b76ff2c153957b7ac82331491
David Henry <david@decoybecoy.com>
2017-01-20 15:48:04 +0000

    Handle multiple results from geocoder lookup
    Filter results returned from postcode geocoding
    to only care when type is ['postal_code'].
    
    This fixes the only occurance of this bug for
    'LG6 2BG'. This process will still fail if multiple
    results are returned which have a type of
    ['postal_code'].

--- app/models/postcode_geocoder.rb
+++ app/models/postcode_geocoder.rb
@@ -2,22 +2,21 @@ require 'geocoder'
 
 class PostcodeGeocoder
   def initialize(postcode)
-    @geocodes = Geocoder.search(postcode)
+    @geocodes = filter_postal_code_result(Geocoder.search(postcode))
   end
 
   def valid?
-    if @geocodes.empty?
-      false
-    elsif @geocodes.count == 1
-      @geocodes.first.data['address_components'].any? { |a| a['types'] == ['postal_code'] }
-    else
-      # add functionality to handle multiple results
-      false
-    end
+    @geocodes.count == 1
   end
 
   def coordinates
     geocode = @geocodes.first
     [geocode.longitude, geocode.latitude]
+  end
+
+  def filter_postal_code_result(geocodes)
+    geocodes.select do |geocode|
+      geocode.data['address_components'].any? { |a| a['types'] == ['postal_code'] }
+    end
   end
 end
