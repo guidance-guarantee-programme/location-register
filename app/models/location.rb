1e45131dd4f9f79f9f5e06da2d32a9646e56cfb7
Ben Lovell <benjamin.lovell@gmail.com>
2017-03-08 13:25:33 +0000

    Separate scopes and class methods with whitespace

--- app/models/location.rb
+++ app/models/location.rb
@@ -55,6 +55,7 @@ class Location < ApplicationRecord # rubocop: disable Metrics/ClassLength
   scope :current, -> { where(state: 'current') }
   scope :active, -> { where(hidden: false) }
   scope :current_by_visibility, -> { current.reorder(:hidden, :title) }
+
   class << self
     def booking_location_for(uid)
       location = includes(:locations, :guiders)
