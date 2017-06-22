8ab6caf369395a824e0fa400acbbd196d15c8a99
Jon Gilbraith <jon@hobocode.io>
2017-06-22 16:01:47 +0100

    Remove unused turbolinks

--- Gemfile
+++ Gemfile
@@ -8,7 +8,6 @@ gem 'sass-rails', '~> 5.0'
 gem 'uglifier', '>= 1.3.0'
 
 gem 'jquery-rails'
-gem 'turbolinks'
 gem 'jbuilder', '~> 2.0'
 
 gem 'foreman'
--- app/assets/javascripts/admin/application.js
+++ app/assets/javascripts/admin/application.js
@@ -12,7 +12,6 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require turbolinks
 //= require ./locations-directory
 //= require select2
 //= require ./search
