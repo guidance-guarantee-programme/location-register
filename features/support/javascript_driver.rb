1292f3bd65ccd86f02c02dd40ae125e1c911c242
Ben Lovell <benjamin.lovell@gmail.com>
2017-10-05 10:19:28 +0100

    Remove phantomjs binaries dependency
    For consistency with our other applications and because
    `phantomjs-binaries` doesn't play nicely with `phantomjs` apps.

--- features/support/javascript_driver.rb
+++ features/support/javascript_driver.rb
@@ -3,6 +3,13 @@ if ENV['WITHOUT_JS']
 else
   require 'capybara/poltergeist'
 
+  Capybara.register_driver :poltergeist do |app|
+    Capybara::Poltergeist::Driver.new(
+      app,
+      phantomjs: Phantomjs.path
+    )
+  end
+
   Capybara.javascript_driver = :poltergeist
   Capybara.default_max_wait_time = 20
 end
