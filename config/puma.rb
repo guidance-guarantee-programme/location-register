5c8292dc4a18def4f17d9f7249b318aa08cb71a6
emilysatodossantos <emily.satodossantos@solirius.com>
2023-11-14 11:42:18 +0000

    Removed script puma

--- config/puma.rb
+++ config/puma.rb
@@ -1,4 +1,3 @@
-#!/usr/bin/env puma
 # Puma can serve each request in a thread from an internal thread pool.
 # The `threads` method setting takes two numbers a minimum and maximum.
 # Any libraries that use thread pools should be configured to match
